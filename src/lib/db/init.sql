c6e974a1ca1ff365963bea6b4434969efd9f8eaf
nagamocha3000 <nagamocha@gmail.com>
2020-03-08 22:38:39 +0300

    move bank_statement fn to main db init file

--- src/lib/db/init.sql
+++ src/lib/db/init.sql
@@ -229,4 +229,43 @@ begin
 end;
 $$;
 
+--BANK STATEMENT
+create function gen_bank_statement(acct_id int)
+    returns table (
+        tx_order bigint,
+        tx_id text,
+        tx_date text,
+        tx_details text,
+        credit numeric,
+        debit numeric,
+        balance numeric
+    )
+    language sql
+as $$
+    with txs as (
+    select
+        substring(transfer_log_id::text,1,8) as tx_id,
+        updated_at,
+        coalesce((select u.firstname || ' ' || u.lastname || ', ' || a.account_id
+                from account a join users u using(user_id)
+                where 
+                    (a.account_id = to_account and from_account = acct_id) or
+                    (a.account_id = from_account and to_account = acct_id)), 'deposit') as tx_details,    
+        case when from_account = acct_id then -amount else amount end as tx_amount
+    from transfer_log
+    where acct_id in (to_account, from_account) and transfer_status = 'confirmed'
+    )select
+        row_number() over (order by updated_at desc) as tx_order, 
+        tx_id,
+        to_char(updated_at, 'dd/mm/yy hh:mm pm') as tx_date,
+        tx_details,
+        case when tx_amount < 0 then -tx_amount else null end as credit,
+        case when tx_amount > 0 then tx_amount else null end as debit,
+        sum (tx_amount) over (
+            order by updated_at asc
+        ) as balance
+    from txs
+    order by updated_at desc;
+$$;
+
 commit;
